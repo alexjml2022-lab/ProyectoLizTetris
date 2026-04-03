548c596dd4f2549f5b95264db47677eb1499eaf8
alexjml2022-lab <alex.jml.2022@gmail.com>
2026-04-03 15:16:02 -0600

    Prueba de cambio de git

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Si ves esto, ¡instalaste todo bien!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Si ves esto, ¡instalaste todo bien!", 191, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
